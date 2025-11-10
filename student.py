4dba29c47624b2ec3246927798c826f337f0f632
veerannadeshanur11 <shreyasdeshanur34@gmail.com>
2025-11-10 12:07:02 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,16 +1,18 @@
 import sys
 #checks if coreect number of argument a
 
-if len(sys.argv) ==3;
+if len(sys.argv) ==3:
     script_name=sys.argv[0]
     name= sys.argy[1]
     rollno=sys.argv[2]
     print("User provide input values:")
+
 else:
     script_name=sys.argv[0]
     name= "Sayed"
     rollno="067"
     print("No Input given -using default values:")
+    
 print("Script Name:",script_name)
 print("Student Name:",name)
 print("Student Rollno:",rollno)
